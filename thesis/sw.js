086a129f71e15008ddd25910f81f9d9643fba00a
ClaudeTraderSG <evergrow11@gmail.com>
2026-06-22 11:54:52 +0800

    thesis: reframe A–F grade → factual 'The Numbers' (neutral, sourced, no buy/avoid verdicts); keep grades private

--- thesis/sw.js
+++ thesis/sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'thesis-v2';
+var CACHE = 'thesis-v3';
 var ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', function (e) {
