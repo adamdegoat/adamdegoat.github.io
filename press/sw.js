54014861c86dee52bb1137b1c6d94d2f0f515c6b
ClaudeTraderSG <evergrow11@gmail.com>
2026-06-29 01:19:54 +0800

    press: weekly drop 2026-06-29

--- press/sw.js
+++ press/sw.js
@@ -1,4 +1,4 @@
-const C='press-v18';
+const C='press-v20';
 self.addEventListener('install',e=>{self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{const u=new URL(e.request.url);
